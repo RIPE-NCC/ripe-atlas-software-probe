72fe0dbe18cbe437be25c688361de65d2e1b65e7
Philip Homburg <phomburg@ripe.net>
2014-04-04 14:11:52 +0200

    Forgot to add atlas_timesync.c.

---
+++ libbb/atlas_timesync.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2013 RIPE NCC <atlas@ripe.net>
+ * Licensed under GPLv2 or later, see file LICENSE in this tarball for details.
+ */
+
+#include "libbb.h"
+int get_timesync(void)
+{
+	FILE *fh;
+	int lastsync;
+
+	fh= fopen(ATLAS_TIMESYNC_FILE, "r");
+	if (!fh)
+		return -1;
+	fscanf(fh, "%d", &lastsync);
+	fclose(fh);
+	return time(NULL)-lastsync;
+}
+
