8c37c4550d25d3788f89b66ff740ea83131b291a
Philip Homburg <phomburg@ripe.net>
2015-11-26 13:22:03 +0100

    Added declaration of get_atlas_fw_version.

--- include/libbb.h
+++ include/libbb.h
@@ -316,6 +316,7 @@ extern int validate_filename(const char *path, const char *prefix);
 extern int validate_atlas_id(const char *atlas_id);
 extern int get_probe_id(void);
 extern int get_timesync(void);
+extern int get_atlas_fw_version(void);
 extern int bind_interface(int socket, int af, char *name);
 extern int atlas_check_addr(const struct sockaddr *sa, socklen_t len);
 
