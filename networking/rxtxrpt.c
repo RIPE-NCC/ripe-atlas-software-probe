086938a87f4b1930ee2e7dbabfbd2e8368f903a4
Philip Homburg <phomburg@ripe.net>
2016-09-13 16:58:51 +0200

    Ethernet statistics in JSON.

--- networking/rxtxrpt.c
+++ networking/rxtxrpt.c
@@ -12,10 +12,14 @@ table if it has changed.
 
 #include "libbb.h"
 
+#define NEW_FORMAT
+
 #define DEV_FILE	"/proc/net/dev"
 #define IF_INET6_FILE	"/proc/net/if_inet6"
 #define IPV6_ROUTE_FILE	"/proc/net/ipv6_route"
 #define SUFFIX		".new"
+
+#define DBQ(str) "\"" #str "\""
 
 int do_atlas= 0;
 
@@ -46,12 +50,30 @@ int rxtxrpt_main(int argc, char *argv[])
 
 	if (do_atlas)
 	{
+#ifdef NEW_FORMAT
+		printf("RESULT { " DBQ(id) ": " DBQ(%s) ", ", opt_atlas);
+		printf(DBQ(fw) ": %d, ", get_atlas_fw_version());
+		printf(DBQ(time) ": %d, ", time(NULL));
+		printf(DBQ(lts) ": %d, ", get_timesync());
+		printf(DBQ(interfaces) ": [");
+#else /* !NEW_FORMWAT */
 		printf("%s %lu ", opt_atlas, time(NULL));
+#endif /* NEW_FORMWAT */
 	}
 
 	r= rpt_rxtx();
 	if (r != 0)
 		return r;
+
+	if (do_atlas)
+	{
+#ifdef NEW_FORMAT
+		printf(" ] }\n");
+#else /* !NEW_FORMAT */
+		printf("\n");
+
+#endif /* NEW_FORMAT */
+	}
 
 	if (cache_name)
 	{
@@ -66,12 +88,101 @@ int rxtxrpt_main(int argc, char *argv[])
 		}
 	}
 
-	if (do_atlas)
-		printf("\n");
-
 	return 0;
 }
 
+#ifdef NEW_FORMAT
+static int rpt_rxtx(void)
+{
+	int i;
+	unsigned long long rx_bytes, rx_packets, rx_errs, rx_drop,
+		rx_fifo, rx_frame, rx_compressed, rx_multicast,
+		tx_bytes, tx_packets, tx_errs, tx_drop,
+		tx_fifo, tx_colls, tx_carrier, tx_compressed;
+	char *cp, *infname;
+	FILE *file;
+	char buf[256];
+
+	file= fopen(DEV_FILE, "r");
+	if (!file)
+	{
+		report_err("unable to open '%s'", DEV_FILE);
+		return 1;
+	}
+
+	/* Skip two lines */
+	if (fgets(buf, sizeof(buf), file) == NULL ||
+		fgets(buf, sizeof(buf), file) == NULL)
+	{
+		report_err("unable to read from '%s'", DEV_FILE);
+		fclose(file);
+		return 1;
+	}
+
+	/* Copy two line */
+	for (i= 0; i<2; i++)
+	{
+		if (fgets(buf, sizeof(buf), file) == NULL)
+		{
+			report_err("unable to read from '%s'", DEV_FILE);
+			fclose(file);
+			return 1;
+		}
+
+		cp= buf;
+
+		/* Skip leading white space */
+		while (*cp == ' ')
+			cp++;
+		infname= cp;
+		cp= strchr(cp, ':');
+		if (cp == NULL)
+		{
+			report_err("format error in '%s'", DEV_FILE);
+			fclose(file);
+			return 1;
+		}
+
+		/* Get all the values */
+		if (sscanf(cp+1, "%llu %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu",
+			&rx_bytes, &rx_packets, &rx_errs, &rx_drop,
+			&rx_fifo, &rx_frame, &rx_compressed, &rx_multicast,
+			&tx_bytes, &tx_packets, &tx_errs, &tx_drop,
+			&tx_fifo, &tx_colls, &tx_carrier, &tx_compressed) != 16)
+		{
+			report_err("format error in '%s'", DEV_FILE);
+			fclose(file);
+			return 1;
+		}
+
+		*cp= '\0';
+
+		printf("%s { " DBQ(infname) ": " DBQ(%s) ", ",
+			i == 0 ? "" : ",", infname);
+	
+		printf(DBQ(rx-bytes) ": %llu, ", rx_bytes);
+		printf(DBQ(rx-packets) ": %llu, ", rx_packets);
+		printf(DBQ(rx-errs) ": %llu, ", rx_errs);
+		printf(DBQ(rx-drop) ": %llu, ", rx_drop);
+		printf(DBQ(rx-fifo) ": %llu, ", rx_fifo);
+		printf(DBQ(rx-frame) ": %llu, ", rx_frame);
+		printf(DBQ(rx-compressed) ": %llu, ", rx_compressed);
+		printf(DBQ(rx-multicast) ": %llu, ", rx_multicast);
+		printf(DBQ(tx-bytes) ": %llu, ", tx_bytes);
+		printf(DBQ(tx-packets) ": %llu, ", tx_packets);
+		printf(DBQ(tx-errs) ": %llu, ", tx_errs);
+		printf(DBQ(tx-drop) ": %llu, ", tx_drop);
+		printf(DBQ(tx-fifo) ": %llu, ", tx_fifo);
+		printf(DBQ(tx-colls) ": %llu, ", tx_colls);
+		printf(DBQ(tx-carrier) ": %llu, ", tx_carrier);
+		printf(DBQ(tx-compressed) ": %llu", tx_compressed);
+		printf(" }");
+	}
+	fclose(file);
+
+	return 0;
+}
+#else /* !NEW_FORMAT */
 static int rpt_rxtx(void)
 {
 	int i;
@@ -120,6 +231,7 @@ static int rpt_rxtx(void)
 
 	return 0;
 }
+#endif /* NEW_FORMAT */
 
 static int setup_ipv6_rpt(char *cache_name, int *need_report)
 {
