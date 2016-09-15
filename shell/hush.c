da415e9a8f2328c905b0e2a04a9c59f4693094b3
Philip Homburg <phomburg@ripe.net>
2016-09-15 12:29:50 +0200

    Convert buddyinfo to JSON.

--- shell/hush.c
+++ shell/hush.c
@@ -102,6 +102,7 @@
 /* In progress... */
 #define ENABLE_HUSH_FUNCTIONS 0
 
+#define DBQ(str) "\"" #str "\""
 
 /* If you comment out one of these below, it will be #defined later
  * to perform debug printfs to stderr: */
@@ -4536,6 +4537,79 @@ static int builtin_epoch (char **argv)
 	return EXIT_SUCCESS;
 }
 
+#define NEW_FORMAT
+
+#ifdef NEW_FORMAT
+static int builtin_buddyinfo(char **argv) 
+{
+	char *lowmemChar;
+	unsigned lowmem = 0;
+	FILE *fp = xfopen_for_read("/proc/buddyinfo");
+	char aa[10];
+	char *my_mac ;
+	int i = 0;
+	int j = 0;
+	int memBlock = 4;
+	int need_reboot = 0; // don't reboot 
+	int freeMem = 0;
+	int jMax = 64; // enough
+	struct sysinfo info; 
+
+	lowmemChar =  argv[1];
+
+	if(lowmemChar) 
+		lowmem = xatou(lowmemChar);
+        fscanf(fp, "%s", aa); 
+        fscanf(fp, "%s", aa);
+        fscanf(fp, "%s", aa);
+        fscanf(fp, "%s", aa);
+
+        my_mac = getenv("ETHER_SCANNED");
+
+	if (lowmem >= 4 ) 
+	{
+		/* We need to reboot unless we find a big enough chunk
+		 * of memory.
+		 */
+		need_reboot = 1;
+	}
+        printf ("RESULT { " DBQ(id) ": " DBQ(9001) ", " DBQ(time) ": %lld",
+		(long long)time(0));
+	if (my_mac !=  NULL)
+		printf(", " DBQ(macaddr) ": " DBQ(%s), my_mac);
+
+	/* get uptime and print it */
+	sysinfo(&info);
+ 	printf (", " DBQ(uptime) ": %ld", info.uptime );
+	
+	printf(", " DBQ(buddyinfo) ": [ ");
+        for (j=0; j < jMax; j++)  
+        {
+                if (fscanf(fp, "%d", &i) != 1)
+			break;
+		printf("%s%d", j == 0 ? "" : ", ", i);
+		freeMem += ( memBlock * i);
+		if (i > 0 && lowmem >= 4 && memBlock >= lowmem)
+		{
+			/* Found a big enough chunk */
+			need_reboot = 0;
+		}
+		memBlock  *= 2; 
+        }
+
+	/* now print it */
+	printf (" ], " DBQ(freemem) ": %d }\n" ,  freeMem);
+
+	fclose (fp);
+
+	if(need_reboot)
+	{
+		fprintf(stderr, "buddy info returned 1 for block %d\n", lowmem);
+		return (EXIT_FAILURE);
+	}
+	return 0;
+}
+#else /* !NEW_FORMAT */
 static int builtin_buddyinfo(char **argv) 
 {
 	char *lowmemChar;
@@ -4625,6 +4699,7 @@ static int builtin_buddyinfo(char **argv)
 	}
 	return 0;
 }
+#endif /* NEW_FORMAT */
 
 static int builtin_findpid(char **argv)
 {
