09e4e209354e2b6ace8fd814d3b1179fd8dd32d8
Philip Homburg <phomburg@ripe.net>
2019-07-29 10:38:59 +0200

    rebased_validated_filename has to accept the new base as well.

--- libbb/validate_filename.c
+++ libbb/validate_filename.c
@@ -34,12 +34,14 @@ int validate_filename(const char *path, const char *prefix)
 
 char *rebased_validated_filename(const char *path, const char *prefix)
 {
-	size_t path_len, prefix_len, atlas_home_len, new_len;
-	char *new_base, *new_path;
+	size_t path_len, prefix_len, atlas_home_len, new_base_len, new_len;
+	int do_replace, failed;
+	const char *new_base;
+	char *new_path;
 
 	/* Check for the following properties:
 	 * 1) path starts with prefix or if prefix is relative,
-	 * '/home/atlas' followed by the prefix.
+	 * ether '/home/atlas' or atlas_base() followed by the prefix.
 	 * 2) the next character after prefix is a '/'
 	 * 3) path does not contain '/../'
 	 * 4) path does not end in '/..'
@@ -49,7 +51,10 @@ char *rebased_validated_filename(const char *path, const char *prefix)
 	path_len= strlen(path);
 	prefix_len= strlen(prefix);
 	atlas_home_len= strlen(ATLAS_HOME);
+	new_base= atlas_base();
+	new_base_len= strlen(new_base);
 
+	do_replace= 0;
 	if (prefix[0] == '/')
 	{
 		if (path_len < prefix_len)
@@ -63,17 +68,46 @@ char *rebased_validated_filename(const char *path, const char *prefix)
 	}
 	else
 	{
+		failed= 0;
+		do_replace= 1;	/* Assume /home/atlas */
 		if (path_len < atlas_home_len + 1 + prefix_len)
-			return NULL;
-		if (memcmp(path, ATLAS_HOME, atlas_home_len) != 0)
-			return NULL;	/* property 1 */
-		if (path[atlas_home_len] != '/')
-			return NULL;	/* property 1 */
-		if (memcmp(path+atlas_home_len+1, prefix, prefix_len) != 0)
-			return NULL;	/* property 1 */
+			failed= 1;
+		if (!failed && memcmp(path, ATLAS_HOME, atlas_home_len) != 0)
+			failed= 1;	/* property 1 */
+		if (!failed && path[atlas_home_len] != '/')
+			failed= 1;	/* property 1 */
+		if (!failed && 
+			memcmp(path+atlas_home_len+1, prefix, prefix_len) != 0)
+		{
+			failed= 1;	/* property 1 */
+		}
 
-		if (path[atlas_home_len+1+prefix_len] != '/')
-			return NULL;	/* property 2 */
+		if (!failed && path[atlas_home_len+1+prefix_len] != '/')
+			failed =1;	/* property 2 */
+
+		if (failed)
+		{
+			do_replace= 0;
+			failed= 0;
+			if (path_len < new_base_len + 1 + prefix_len)
+				failed= 1;
+			if (!failed &&
+				memcmp(path, new_base, new_base_len) != 0)
+			{
+				failed= 1;	/* property 1 */
+			}
+			if (!failed && path[new_base_len] != '/')
+				failed= 1;	/* property 1 */
+			if (!failed && 
+				memcmp(path+new_base_len+1, prefix,
+				prefix_len) != 0)
+			{
+				failed= 1;	/* property 1 */
+			}
+		}
+
+		if (failed)
+			return NULL;
 	}
 
 	if (strstr(path, "/../") != NULL)
@@ -82,11 +116,15 @@ char *rebased_validated_filename(const char *path, const char *prefix)
 	if (path_len >= 3 && strcmp(&path[path_len-3], "/..") == 0)
 		return NULL;	/* property 4 */
 	
-	new_base= atlas_base();
-	new_len= strlen(new_base) + (path_len-atlas_home_len) + 1;
-	new_path= xmalloc(new_len);
-	strlcpy(new_path, new_base, new_len);
-	strlcat(new_path, path+atlas_home_len, new_len);
+	if (do_replace)
+	{
+		new_len= strlen(new_base) + (path_len-atlas_home_len) + 1;
+		new_path= xmalloc(new_len);
+		strlcpy(new_path, new_base, new_len);
+		strlcat(new_path, path+atlas_home_len, new_len);
+	}
+	else
+		new_path= strdup(path);
 
 	return new_path;
 }
