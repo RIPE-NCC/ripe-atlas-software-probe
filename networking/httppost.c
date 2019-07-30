3417c5d6a0c0ae7f7437bed9666d6867a97b6c42
Philip Homburg <phomburg@ripe.net>
2019-07-30 15:56:29 +0200

    Use rebased_validated_dir for post-dir.

--- networking/httppost.c
+++ networking/httppost.c
@@ -338,13 +338,8 @@ int httppost_main(int argc, char *argv[])
 
 	if (post_dir)
 	{
-		rebased_fn= rebased_validated_filename(post_dir,
+		rebased_fn= rebased_validated_dir(post_dir,
 			SAFE_PREFIX_DATA_OUT_REL);
-		if (rebased_fn == NULL)
-		{
-			rebased_fn= rebased_validated_filename(post_dir,
-				SAFE_PREFIX_STATUS_REL);
-		}
 		if (rebased_fn == NULL)
 		{
 			report("protected dir (post) '%s'", post_dir);
