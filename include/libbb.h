228de0a52b001a39f5dc0dbb43726b440cc69332
Philip Homburg <phomburg@ripe.net>
2019-07-30 15:56:09 +0200

    rebased_validated_dir

--- include/libbb.h
+++ include/libbb.h
@@ -452,8 +452,8 @@ char *is_suffixed_with(const char *string, const char *key) FAST_FUNC;
 #define ATLAS_TIMESYNC_FILE_REL    ATLAS_DATA_NEW_REL "/timesync.vol"
 #define ATLAS_FUZZING_REL          "data"
 
-extern int validate_filename(const char *path, const char *prefix);
-char *rebased_validated_filename(const char *path, const char *prefix);
+extern char *rebased_validated_filename(const char *path, const char *prefix);
+extern char *rebased_validated_dir(const char *path, const char *prefix);
 extern int validate_atlas_id(const char *atlas_id);
 extern int get_probe_id(void);
 extern int get_timesync(void);
--- libbb/validate_filename.c
+++ libbb/validate_filename.c
@@ -1,38 +1,7 @@
 #include "libbb.h"
 
-int validate_filename(const char *path, const char *prefix)
-{
-	size_t path_len, prefix_len;
-
-	/* Check for the following properties:
-	 * 1) path start with prefix
-	 * 2) the next character after prefix is a '/'
-	 * 3) path does not contain '/../'
-	 * 4) path does not end in '/..'
-	 * return 0 if any of the properties does not hold
-	 * return 1 if all properties hold
-	 */
-	path_len= strlen(path);
-	prefix_len= strlen(prefix);
-	if (path_len < prefix_len)
-		return 0;
-
-	if (memcmp(path, prefix, prefix_len) != 0)
-		return 0;	/* property 1 */
-
-	if (path[prefix_len] != '/')
-		return 0;	/* property 2 */
-
-	if (strstr(path, "/../") != NULL)
-		return 0;	/* property 3 */
-
-	if (path_len >= 3 && strcmp(&path[path_len-3], "/..") == 0)
-		return 0;	/* property 4 */
-	
-	return 1;
-}
-
-char *rebased_validated_filename(const char *path, const char *prefix)
+static char *rebased_validated_common(const char *path, const char *prefix,
+	int require_slash)
 {
 	size_t path_len, prefix_len, atlas_home_len, new_base_len, new_len;
 	int do_replace, failed;
@@ -42,7 +11,8 @@ char *rebased_validated_filename(const char *path, const char *prefix)
 	/* Check for the following properties:
 	 * 1) path starts with prefix or if prefix is relative,
 	 * ether '/home/atlas' or atlas_base() followed by the prefix.
-	 * 2) the next character after prefix is a '/'
+	 * 2) the next character after prefix is a '/' (or '\0' if
+	 *    require_slash is false)
 	 * 3) path does not contain '/../'
 	 * 4) path does not end in '/..'
 	 * return NULL if any of the properties does not hold
@@ -64,7 +34,10 @@ char *rebased_validated_filename(const char *path, const char *prefix)
 			return NULL;	/* property 1 */
 
 		if (path[prefix_len] != '/')
-			return NULL;	/* property 2 */
+		{
+			if (require_slash || path[prefix_len] != '\0')
+				return NULL;	/* property 2 */
+		}
 	}
 	else
 	{
@@ -83,7 +56,13 @@ char *rebased_validated_filename(const char *path, const char *prefix)
 		}
 
 		if (!failed && path[atlas_home_len+1+prefix_len] != '/')
-			failed =1;	/* property 2 */
+		{
+			if (require_slash ||
+				path[atlas_home_len+1+prefix_len] != '\0')
+			{
+				failed =1;	/* property 2 */
+			}
+		}
 
 		if (failed)
 		{
@@ -103,6 +82,17 @@ char *rebased_validated_filename(const char *path, const char *prefix)
 				prefix_len) != 0)
 			{
 				failed= 1;	/* property 1 */
+			}
+
+			if (!failed && path[atlas_home_len+1+prefix_len] !=
+				'/')
+			{
+				if (require_slash ||
+					path[atlas_home_len+1+prefix_len] !=
+					'\0')
+				{
+					failed =1;	/* property 2 */
+				}
 			}
 		}
 
@@ -127,4 +117,14 @@ char *rebased_validated_filename(const char *path, const char *prefix)
 		new_path= strdup(path);
 
 	return new_path;
+}
+
+char *rebased_validated_filename(const char *path, const char *prefix)
+{
+	return rebased_validated_common(path, prefix, 1 /*require_slash*/);
+}
+
+char *rebased_validated_dir(const char *path, const char *prefix)
+{
+	return rebased_validated_common(path, prefix, 0 /*!require_slash*/);
 }
