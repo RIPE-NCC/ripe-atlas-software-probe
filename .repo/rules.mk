686aa5b8100f77699c5c7d9f68efc4ada13bab48
ttaiclet <ttaiclet@ripe.net>
2025-10-14 21:39:25 +0200

    changed: improve Debian .repo rules.mk & fix tag bug
    Add comments for clarity.
    Add support for non 'devel.*' (e.g. 'devel-' or 'anything').
    Add support for tags.
    
    Reviewed-by: Marios Mavropoulos <mmavropoulos@ripe.net>

--- .repo/rules.mk
+++ .repo/rules.mk
@@ -1,20 +1,53 @@
-# Try and default to GitLab CI/CD variables
-BRANCH:=$(CI_COMMIT_TAG)
+# Set BRANCH
+
+ifeq ($(GITLAB_CI),true)
+	# In CI
+	ifneq ($(CI_COMMIT_TAG),)
+		# CI tag run
+		BRANCH:=$(CI_COMMIT_TAG)
+		# Does the tag start with a digit? (e.g. 5120)
+		ifneq ($(filter 0% 1% 2% 3% 4% 5% 6% 7% 8% 9%,$(BRANCH)),)
+			RELEASE:=master
+		else
+$(warning Unknown tag format. Typically starts with digit (e.g. 5120) and is master branch.)
+		endif
+	else ifneq ($(CI_COMMIT_BRANCH),)
+		# CI branch run
+		BRANCH:=$(CI_COMMIT_BRANCH)
+	else
+$(error ERROR: Running in CI, and there is no CI_COMMIT_TAG or CI_COMMIT_BRANCH.)
+	endif
+else
+	# No CI - get branch manually
+	BRANCH:=$(shell git rev-parse --abbrev-ref HEAD 2>/dev/null)
+endif
+
+# Set RELEASE based on Git branch
+
+ifeq ($(BRANCH),master)
+	RELEASE:=master
+endif
+ifneq ($(filter testing%,$(BRANCH)),) # testing, testing-hello, testing.hello2, ... are all valid
+	RELEASE:=testing
+endif
+ifneq ($(filter devel%,$(BRANCH)),) # devel, devel-hello, devel.hello2, ... are all valid
+	RELEASE:=devel
+endif
+ifeq ($(RELEASE),) # devel is a catch-all (e.g. unknown tags/branches)
+$(warning Branch could not be matched with a release, defaulting to devel)
+	RELEASE:=devel
+endif
+
+$(info Using BRANCH "$(BRANCH)")
+$(info Using RELEASE "$(RELEASE)")
 ifeq ($(BRANCH),)
-BRANCH:=$(CI_COMMIT_BRANCH)
+$(error ERROR: No branch was matched)
+else ifeq ($(RELEASE),)
+$(error ERROR: No release was matched)
 endif
 
-# Derive from GIT if none available (local build?)
-ifeq ($(BRANCH),)
-BRANCH:=$(shell git rev-parse --abbrev-ref HEAD 2>/dev/null)
-endif
-
-# Default to the master branch
-RELEASE:=$(firstword $(subst ., ,$(BRANCH)))
-ifeq ($(RELEASE),)
-RELEASE:=master
-endif
-
+# If the RELEASE is not master, we need to add something in the URL
+# (...)/software-probe/.testing/ and (...)/software-probe/.devel/
 ifneq ($(RELEASE),master)
 REPO:=.$(RELEASE)/
 else
